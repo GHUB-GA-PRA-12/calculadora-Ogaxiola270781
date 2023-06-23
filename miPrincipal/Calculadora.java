38aa1656e2b19d37acb7ffee2e8c42683e5f0a56
Omar Ivan Gaxiola Sánchez <136842959+Ogaxiola270781@users.noreply.github.com>
2023-06-23 20:11:40 +0000

    resuelto

--- miPrincipal/Calculadora.java
+++ miPrincipal/Calculadora.java
@@ -3,18 +3,18 @@ package miPrincipal;
 public class Calculadora {
 
     public int suma(int x, int y) {
-        return 0;
+        return x+y;
     }
     
     public int resta(int x, int y) {
-        return 0;
+        return x-y;
     }
 
     public int multiplica(int x, int y) {
-        return 0;
+        return x*y;
     }
 
     public int divide(int x, int y) {
-        return 0;
+        return x/y;
     }
 }
